e901f64bdf177d5b72c8ca8ee6387f5570d3a26f
HponeThitHtoo <white.nuzzle@gmail.com>
2023-06-21 13:50:43 +0630

    first commit

---
+++ myTest/index.js
@@ -0,0 +1,5 @@
+// Global app controller
+import str from './models/Search';
+import { add, multiply, ID} from './views/searchView';
+
+console.log(`Using imported functions! ${add(ID, 2)} and ${multiply(3, 5)}. ${str}`);
